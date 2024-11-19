c7e1ff8cac80c0326d39774ede03702a40cde58c
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-11-19 16:39:53 +0900

    GameInstance와 GameMode
    1. GameInstance와 GameMode를 c++로만듬
    2. GameInstance와 GameMode를 블루프린터상속받음
    3. 게임 세팅에서 교체
    
    GameInstance - GameInstance는 게임이 시작될 때 생성되고, 게임이 종료될 때까지 전체적으로 유지되는 싱글톤 클래스입니다. 게임의 글로벌 상태를 관리하는 데 사용됩니다. ex) 게임 전역 데이터를 저장하고 관리하기에 적합합니다.,  플레이어 이름, 설정, 경험치. 네트워크 상태
    GameMode - 현재 레벨의 게임 규칙과 흐름을 정의하는 클래스입니다. 게임의 상태와 플레이어 행동을 제어하는 역할을 합니다. PlayerController, PlayerState, Pawn 등 클래스의 기본 유형을 설정합니다.

---
+++ R1/Source/R1/System/R1GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "R1GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API UR1GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
