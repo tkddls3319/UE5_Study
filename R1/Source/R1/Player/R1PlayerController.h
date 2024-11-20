74936001744c1d8da2851503b07a7cd0d1b668e3
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-11-20 16:31:05 +0900

    Pawn
    1. 새롭게 시작하기 위해 그동안 테스트용으로 만들었던 MainActor, R1Actor, R1Obejct, R1Editor폴더 삭제
    
    2. C++ R1Pawn 추가 후 상속받아 블루프린터를 추가
    
    3. WorldSetting에서 Player를 블루프린터 R1Pawn으로 선택 (블루프린터 안에서도 R1Pawn을 Player로 설정가능)
    
    4. C++ PlayerController 생성 및 블루프린터 상속으로 생성
    
    5. 다른 폴더에 C++생성 할 때 마다 헤더 경로가 안맞는 문제는 R1.Build.cs에서 PublicIncludePaths.AddRange에 최상위 경로설정으로 해결가능
    
    6. 블루프린터 BP_R1Pawn에 Cpsule, SkeletaMesh, SpringArm, Camera 설정
    
    7. 블루프린터에 설정한 내용을 R1Pawn C++로 옮긴다.
    
    ------------------------------------------
    Pawn부터는 기존 Actor와 다르게 SetupPlayerInputComponent가 있어 사용자 입력을 받을 수 있지만 PlayerController에 입력을 받는게 권장된다.

---
+++ R1/Source/R1/Player/R1PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "R1PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API AR1PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
