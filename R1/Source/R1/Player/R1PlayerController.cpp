e4e2d397e579497d68faecdaee102ff67ef83704
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-11-21 16:45:20 +0900

    PlayerControlRotation와 MovementComponent
    1. 이전에 직접적으로 월드상 좌표를 조정했지만 이번엔 MovementComponent에게 일감을 줘서 처리하는 방식으로 변경하고 PlayerController의 회전을 이용한 방법을 진행해봄. AddMovementInput을 사용할 때 블루 프린터에서 movenment component추가해줘야함.
    
    *Pawn에 포함된 AddMovementInput의 개념은 아주중요하다. 캐릭터의 입력을 처리하여 이동 방향을 설정하는 함수로 이동을 내리면 MovementComponent에서 모든 이동관련 행동을 실제로 수행하게된다. 즉 AddMovementInput는 입력을 이동 방향 벡터로 변환하여 캐릭터의 이동 명령을 예약하는 함수입니다.
    *PlayerController에서는 입력과 회전정보를 제공한다. GetControlRotation()을 호출하면 PlayerController에서 저장하고있는 Rotation을 전달한다.
    AddYawInput()는 PlayerController의 Yaw 값을 연구적으로 상태를 변경한다.
    Pawn블루프린터에보면 Use Controller Rotation Yaw와 같은 체크박스란이 존재한다. 이 박스를 선택하면 Pawn의 회전값이 PlayerController의 회전값과 동일하게 설정된다는 뜻 이다.
    
    ------------------------------------------
    GetControlRotation()
    UKismetMathLibrary::GetForwardVector
    GetPawn()->AddMovementInput
    AddYawInput()

--- R1/Source/R1/Player/R1PlayerController.cpp
+++ R1/Source/R1/Player/R1PlayerController.cpp
@@ -34,7 +34,6 @@ void AR1PlayerController::SetupInputComponent()
 }
 void AR1PlayerController::Input_Test(const FInputActionValue& InputValue) 
 {
-	//화명상의 로그찍는 방법
 	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor::Cyan, TEXT("TEST"));
 }
 
@@ -44,18 +43,35 @@ void AR1PlayerController::Input_Move(const FInputActionValue& InputValue)
 
 	if (MovementVector.X != 0)
 	{
-		FVector Direction = FVector::ForwardVector * MovementVector.X;
-		GetPawn()->AddActorWorldOffset(Direction * 50.0f);
+		//AddActorWorldOffset 해당버전은 직접적으로 월드좌표를 이용하여 움직인다. 이 때 대각선같은 부분에 대한 연산은 따로 만들어줘야한다.
+		/*FVector Direction = FVector::ForwardVector * MovementVector.X;
+		GetPawn()->AddActorWorldOffset(Direction * 50.0f);*/
+
+		/*
+		Unreal Engine에서 **AddMovementInput**와 Movement(정확히는 CharacterMovementComponent)는 서로 관련이 깊다.
+		AddMovementInput은 캐릭터의 입력을 처리하여 이동 방향을 설정하는 함수이고, 
+		이동은 **CharacterMovementComponent**가 실제로 처리합니다.
+		즉 AddMovementInput는 입력을 이동 방향 벡터로 변환하여 캐릭터의 이동 명령을 예약하는 함수입니다.
+		CharacterMovementComponent는 예약된 이동 명령을 처리하는 컴포넌트이다.
+		*/
+		FRotator Rotator = GetControlRotation();//PlayerController에 입력된 방향
+		FVector Direction = UKismetMathLibrary::GetForwardVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.X);
 	}
 	if (MovementVector.Y != 0)
 	{
-		FVector Direction = FVector::RightVector * MovementVector.Y;
-		GetPawn()->AddActorWorldOffset(Direction * 50.0f);
+	/*	FVector Direction = FVector::ForwardVector * MovementVector.Y;
+		GetPawn()->AddActorWorldOffset(Direction * 50.0f);*/
+
+		FRotator Rotator = GetControlRotation();
+		FVector Direction = UKismetMathLibrary::GetRightVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.Y);
 	}
+
 }
 
 void AR1PlayerController::Input_Turn(const FInputActionValue& InputValue)
 {
 	float Val = InputValue.Get<float>();
-	AddYawInput(Val);
+	AddYawInput(Val);//연구적으로 로테이션의 상태가 PlayerController에 보존된다.
 }
