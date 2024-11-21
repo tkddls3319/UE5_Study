55221ebdc45d22bca7edf8064bdf8d6fe8f65e20
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-11-21 14:07:32 +0900

    Input (Input Mapping Context와 InputAction )
    1. InputAction 생성 ( Test, Move, Turn ) Input Action은 특정 입력 동작(예: 점프, 이동)을 추상화한 객체입니다.
    
    2. Input Mapping Context 생성
     앞에서만든 여러 Input Action을 입력 장치(키보드, 마우스, 게임패드 등)에 연결할 수 있습니다.
    
    3. BP_R1Pawn에서 블루프린트로 Input Mapping Context와 inputAction을 사용하는 법
    
    4. InputMappingContext에서 등록한 Input Action의 키를 눌렀을 때 음수가 나오게 하는 방법 modifiers에서 Negate를 추가 하면 음수로 변경된다, 기존 x,y를 y,x로 변경 하는 방법 modifiers에 Swizzle Input Axis Values를 추가한다.
    
    5. c++에서 Input을 사용하기위해서는 EnhancedInput의 모듈을 추가한다.
    
    6. AR1PlayerController의 기본생성자와 beginplay, setupinputcomponent를 override한다. Input같은 경우에는 PlayerController에 만드는 것 이 좋다. PlayerController에 기존에 만들었던InputMappingcontext와 InputAction을 담을 변수들과 입력이 받아졌을 때 어떤 행위를 정의할 콜백함수를 만든다.
    
    ------------------------------------------
    #include "EnhancedInputComponent.h"
    #include "EnhancedInputSubsystems.h"
    #include "Kismet/KismetMathLibrary.h"
    void Input_Move(const FInputActionValue& InputValue);(콜백용)
    UInputMappingContext
    UInputAction
    ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer())
    AddMappingContext
    UEnhancedInputComponent
    GEngine->AddOnScreenDebugMessage

--- R1/Source/R1/Player/R1PlayerController.cpp
+++ R1/Source/R1/Player/R1PlayerController.cpp
@@ -2,4 +2,60 @@
 
 
 #include "R1PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/KismetMathLibrary.h"
+AR1PlayerController::AR1PlayerController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
 
+void AR1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+	}
+}
+
+void AR1PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (auto* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(TestAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Test);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Turn);
+	}
+
+}
+void AR1PlayerController::Input_Test(const FInputActionValue& InputValue) 
+{
+	//화명상의 로그찍는 방법
+	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor::Cyan, TEXT("TEST"));
+}
+
+void AR1PlayerController::Input_Move(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	if (MovementVector.X != 0)
+	{
+		FVector Direction = FVector::ForwardVector * MovementVector.X;
+		GetPawn()->AddActorWorldOffset(Direction * 50.0f);
+	}
+	if (MovementVector.Y != 0)
+	{
+		FVector Direction = FVector::RightVector * MovementVector.Y;
+		GetPawn()->AddActorWorldOffset(Direction * 50.0f);
+	}
+}
+
+void AR1PlayerController::Input_Turn(const FInputActionValue& InputValue)
+{
+	float Val = InputValue.Get<float>();
+	AddYawInput(Val);
+}
