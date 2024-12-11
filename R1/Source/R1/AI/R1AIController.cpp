8b5a2fa5c0dfc6aea505a9e6779cac84a622db93
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-12-11 10:56:59 +0900

    AI#1 - AIController, NavMeshBoundsVolume을 사용한 기본적인 길찾기
    AIController를 상속받아 Character에 적용하는 방법 학습
    
    길 찾기를 위해 NavMeshBoundsVolume 사용
    
    AI에게 목적지를 줘서 이동하게 소스코드 사용법
    
    *AIController 설정
    1. C++로 AIController를 상속 받는 R1AIController를 생성한다.
    2. 생성자와 OnPossess 함수를 오버라이드해준다.
    3. 기존에 블루프린터로 만들었던 Character의 Details에 Pawn 영역을 보면 Auto Possess Player가 Disabled로 되어있다면 AI Controller Class에 적용된 AIController에 의해 AI가 작동한다. 이 AI Controller Class를 앞서 만들었던 R1AIController로 변경해 주면 상속받아 만든 AIController를 사용할 수 있다.
    그리고 Auto Possess AI는 어떤 방식으로 만든 Character에 대해 AI를 적용할지 결정함 Placed in World는 에디터에서 드래그 드롭으로 만든 Character, Spawn은 C++로 만든 Character, Placed in World or Spawnd는 둘 다이기 때문에 그냥 편하게 마지막 것 선택
    
    4. R1AIController를 상속받는 블루프린트 BP_R1MonsterController를 하나 만들어 준다.
    5. 몬스터로 지정할 Character에 가서 AI Controller Class에 BP_R1MonsterController를 적용한다.
    
    *NavMeshBoundsVolume
    1. NavMeshBoundsVolume을 에디터에 놓는다. 단축키 P 누르면 이동 가능 영역 표시됨
    
    *AIController를 이용한 AI 이동
    1. AR1AIController의 BeginPlay안에 FAIMoveRequest와 MoveTo를 사용하여 목적지를 입력하는 코드를 넣어준다.
    2. MoveTo에서 어떤 경로로 이동했는지를 FNavPathSharedPtr를 사용하여 받아 로그를 찍어준다.

---
+++ R1/Source/R1/AI/R1AIController.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/R1AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+AR1AIController::AR1AIController(const FObjectInitializer& ObjectInitializer )
+	: Super(ObjectInitializer)
+{
+
+}
+
+void AR1AIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+void AR1AIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FVector Dest = { 0,0,0 };
+	FAIMoveRequest MoveRequest;
+	MoveRequest.SetGoalLocation(Dest);//목적지
+	MoveRequest.SetAcceptanceRadius(15.f);//거리 15만큼 이면 도착했다고 인정
+
+	FNavPathSharedPtr NavPath;// 경로 로그를 위해
+	MoveTo(MoveRequest, OUT& NavPath);
+
+	//로그 찍는 디버그 코드
+	if (NavPath.IsValid())
+	{
+		TArray<FNavPathPoint>& PathPoints =  NavPath->GetPathPoints();
+		
+		for (const auto& Point : PathPoints )
+		{
+			const FVector& Location = Point.Location;
+			DrawDebugSphere(GetWorld(), Location, 12.f, 12, FColor::Green, false, 10.0f);
+		}
+	}
+}
+
+void AR1AIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
